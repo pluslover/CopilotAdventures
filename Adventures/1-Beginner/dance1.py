16f42d8276b388663d0eace8cc649d195ae431e9
pluslover <pluslover@qq.com>
2024-04-22 09:53:18 +0000

    refator dance1

--- Adventures/1-Beginner/dance1.py
+++ Adventures/1-Beginner/dance1.py
@@ -1,8 +1,10 @@
-#ask user to input a string from console to insert a entry into dictionary, each string are , delimtered. key is the combination of the two strings with _ as separator, value is the third string
-#repeat the process until user input a empty string
-#after user input all the strings, print out the dictionary
+# Ask the user to input a string from the console to insert an entry into the dictionary.
+# Each string is delimited by commas. The key is the combination of the first two strings with '_' as the separator, and the value is the third string.
+# Repeat the process until the user inputs an empty string.
+# After the user inputs all the strings, print out the dictionary.
 
 dance = {}
+
 while True:
     entry = input('Enter a string to insert into the dictionary (key1,key2,value): ')
     if entry == '':
@@ -10,27 +12,18 @@ while True:
     key1, key2, value = entry.split(',')
     dance[key1 + '_' + key2] = value
 
-dance1_sequence = [];
-dance2_sequence = [];
-while True:
-    dance1 = input('Enter the first dance sequence: ')
-    if dance1 == '':
-        break
-    dance1_sequence.append(dance1)
+dance1_sequence = input('Enter the first dance sequence: ').split(',')
+dance2_sequence = input('Enter the second dance sequence: ').split(',')
 
-# ask user to input the second dance sequence
-while True:
-    dance2 = input('Enter the second dance sequence: ')
-    if dance2 == '':
-        break
-    dance2_sequence.append(dance2)
+# Calculate the forest status based on dance1_sequence and dance2_sequence.
+# Each dance will affect the forest status.
 
-# calculate the forest status based on dance1_sequence and dance2_sequence, each dance will affect the forest status
 forest_status_changes = [dance[d1 + '_' + d2] for d1, d2 in zip(dance1_sequence, dance2_sequence)]
-# print out the forest status changes
+
+# Print out the forest status changes.
+
 for d1, d2, status in zip(dance1_sequence, dance2_sequence, forest_status_changes):
     print(f'Dance 1: {d1}, Dance 2: {d2}, Forest Status: {status}')
-
 
 
 
